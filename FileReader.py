bdb5aaf14ff1d7a80cf5d1f5323c1847fee33fd5
Quinn0809 <s3724287@student.rmit.edu.au>
2020-07-14 14:38:01 +1000

    Update FileReader.py

--- FileReader.py
+++ FileReader.py
@@ -4,7 +4,7 @@ import pandas as pd
 read_file = pd.read_excel (r'/Users/quinn/Desktop/01_Desktop/02 Projects/CueIt/EBTYH Kylie Music EDL.xlsx')
 read_file.to_csv (r'/Users/quinn/Desktop/01_Desktop/02 Projects/CueIt/input.csv', index = None, header=True)
 
-text = open("input.csv", "r")
+text = open("/Users/quinn/Desktop/01_Desktop/02 Projects/CueIt/input.csv", "r")
 text = ''.join([i for i in text]).replace("_", " ") \
          .replace("copy.01.new.01", "" ) \
          .replace("bounced", "" ) 
